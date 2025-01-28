49cf3c25727eedd37d7371c46796ea3d354caefd
John Lewis <github@jlewis.sh>
2025-01-28 14:00:19 +0100

    fix(repos): switch store repo to concrete type

--- crates/repos/src/store.rs
+++ crates/repos/src/store.rs
@@ -1,5 +1,7 @@
 //! Provides a repository for the [`Store`] domain model.
 
+use std::sync::Arc;
+
 use hex::health::{self, HealthAware};
 pub use models::{Store, StoreCreateRequest};
 use tracing::instrument;
@@ -8,45 +10,73 @@ use super::*;
 pub use crate::base::CreateModelError;
 use crate::base::{BaseRepository, Database};
 
-/// Descriptor trait for repositories that handle [`Store`] domain model.
-#[async_trait::async_trait]
-pub trait StoreRepository:
-  ModelRepositoryLike<
-  Model = Store,
-  ModelCreateRequest = StoreCreateRequest,
-  CreateError = CreateModelError,
->
-{
-}
-
-impl<T> StoreRepository for T where
-  T: ModelRepositoryLike<
-    Model = Store,
-    ModelCreateRequest = StoreCreateRequest,
-    CreateError = CreateModelError,
-  >
-{
-}
-
 /// The repository for the [`Store`] domain model.
-#[derive(Clone)]
-pub struct StoreRepositoryCanonical {
-  base_repo: BaseRepository<Store>,
+pub struct StoreRepository {
+  inner: Arc<
+    dyn ModelRepositoryLike<
+      Model = Store,
+      ModelCreateRequest = Store,
+      CreateError = CreateModelError,
+    >,
+  >,
 }
 
-impl StoreRepositoryCanonical {
-  /// Create a new instance of the [`Store`] repository.
-  pub fn new(db: Database<Store>) -> Self {
-    tracing::info!("creating new `StoreRepositoryCanonical` instance");
+impl StoreRepository {
+  /// Creates a new instance of the [`Store`] repository using `BaseRepository`.
+  pub fn new_from_base(db: Database<Store>) -> Self {
     Self {
-      base_repo: BaseRepository::new(db),
+      inner: Arc::new(BaseRepository::new(db)),
     }
+  }
+
+  /// Creates a new model.
+  #[instrument(skip(self))]
+  async fn create_model(
+    &self,
+    input: StoreCreateRequest,
+  ) -> Result<Store, CreateModelError> {
+    self.inner.create_model(input.into()).await
+  }
+
+  /// Fetches a model by its ID.
+  #[instrument(skip(self))]
+  async fn fetch_model_by_id(
+    &self,
+    id: models::RecordId<Store>,
+  ) -> Result<Option<Store>, FetchModelError> {
+    self.inner.fetch_model_by_id(id).await
+  }
+
+  /// Fetches a model by an index.
+  ///
+  /// Must be a valid index, defined in the model's `INDICES` constant.
+  #[instrument(skip(self))]
+  async fn fetch_model_by_index(
+    &self,
+    index_name: String,
+    index_value: EitherSlug,
+  ) -> Result<Option<Store>, FetchModelByIndexError> {
+    self
+      .inner
+      .fetch_model_by_index(index_name, index_value)
+      .await
+  }
+
+  /// Produces a list of all model IDs.
+  #[instrument(skip(self))]
+  async fn enumerate_models(&self) -> Result<Vec<Store>> {
+    self.inner.enumerate_models().await
   }
 }
 
-crate::impl_repository_on_base!(
-  StoreRepositoryCanonical,
-  Store,
-  StoreCreateRequest,
-  CreateModelError
-);
+#[async_trait::async_trait]
+impl health::HealthReporter for StoreRepository {
+  fn name(&self) -> &'static str { stringify!(StoreRepository) }
+  async fn health_check(&self) -> health::ComponentHealth {
+    health::AdditiveComponentHealth::from_futures(Some(
+      self.inner.health_report(),
+    ))
+    .await
+    .into()
+  }
+}
