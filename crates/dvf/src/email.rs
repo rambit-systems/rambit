4d11397b899fa325b4d8c4ecfc484bd682075806
John Lewis <github@jlewis.sh>
2025-04-02 18:37:06 +0200

    fix(dvf): feature-guard regex in dvf

--- crates/dvf/src/email.rs
+++ crates/dvf/src/email.rs
@@ -1,9 +1,11 @@
 use std::{str::FromStr, sync::LazyLock};
 
+#[cfg(feature = "reasonable-email")]
 /// Our "reasonable" email regex is significantly more restrictive than the
 /// RFCs. sourced from https://colinhacks.com/essays/reasonable-email-regex,
 /// with lowercase ranges added.
 const EMAIL_REASONABLE_REGEX_STRING: &str = r#"^([A-Za-z0-9_+-]+\.?)*[A-Za-z0-9_+-]@([A-Za-z0-9][A-Za-z0-9-]*\.)+[A-Za-z]{2,}$"#;
+#[cfg(feature = "reasonable-email")]
 static EMAIL_REASONABLE_REGEX: LazyLock<regex::Regex> =
   LazyLock::new(|| regex::Regex::new(EMAIL_REASONABLE_REGEX_STRING).unwrap());
 
@@ -12,6 +14,7 @@ pub fn validate_compliant_email_address(email: &str) -> bool {
   email_address::EmailAddress::from_str(email).is_ok()
 }
 
+#[cfg(feature = "reasonable-email")]
 /// Validate that an email address is reasonable.
 pub fn validate_reasonable_email_address(email: &str) -> bool {
   EMAIL_REASONABLE_REGEX.is_match(email)
@@ -34,6 +37,7 @@ pub fn validate_reasonable_email_address(email: &str) -> bool {
 pub struct EmailAddress(String);
 
 impl EmailAddress {
+  #[cfg(feature = "reasonable-email")]
   /// Check if the email address is reasonable.
   pub fn is_reasonable(&self) -> bool {
     validate_reasonable_email_address(self.as_ref())
@@ -50,7 +54,9 @@ mod tests {
     let compliant_email = "main@[192.168.0.1]";
     assert!(validate_compliant_email_address(compliant_email));
     assert!(validate_compliant_email_address(reasonable_email));
+    #[cfg(feature = "reasonable-email")]
     assert!(validate_reasonable_email_address(reasonable_email));
+    #[cfg(feature = "reasonable-email")]
     assert!(!validate_reasonable_email_address(compliant_email));
   }
 }
