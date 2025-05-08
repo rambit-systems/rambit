3f5e874bfc62447915221e5a3b8327bde3ccf21f
John Lewis <github@jlewis.sh>
2025-05-08 12:07:07 +0200

    feat(models): empty `models` except for `Org`

--- crates/models/src/lib.rs
+++ crates/models/src/lib.rs
@@ -1,17 +1,9 @@
 //! Provides domain model types. Used by most crates in the workspace.
 
-mod cache;
-mod entry;
 mod org;
-mod perms;
-mod store;
-mod token;
-mod user;
 
 pub use dvf::*;
 pub use model::*;
 pub use slugger::*;
 
-pub use self::{
-  cache::*, entry::*, org::*, perms::*, store::*, token::*, user::*,
-};
+pub use self::org::*;
--- crates/models/src/org.rs
+++ crates/models/src/org.rs
@@ -2,28 +2,23 @@ use serde::{Deserialize, Serialize};
 
 use crate::{Model, RecordId};
 
-/// The [`Org`] table name.
-pub const ORG_TABLE_NAME: &str = "org";
-
-/// An org record ID.
-pub type OrgRecordId = RecordId<Org>;
-
 /// An org.
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Org {
   /// The org's ID.
-  pub id:   OrgRecordId,
+  pub id:   RecordId<Org>,
   /// The org's name.
   pub name: dvf::EntityName,
 }
 
 impl Model for Org {
-  const TABLE_NAME: &'static str = ORG_TABLE_NAME;
+  const TABLE_NAME: &'static str = "org";
+
   const UNIQUE_INDICES: &'static [(
     &'static str,
     crate::SlugFieldGetter<Self>,
   )] = &[("name", |org| org.name.clone().into_inner().into())];
   const INDICES: &'static [(&'static str, model::SlugFieldGetter<Self>)] = &[];
 
-  fn id(&self) -> OrgRecordId { self.id }
+  fn id(&self) -> RecordId<Org> { self.id }
 }
