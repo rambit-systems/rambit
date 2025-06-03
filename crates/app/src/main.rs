0f7bba0fda092cb3cee1ea5e7c8e68fa3fe5dcdf
John Lewis <github@jlewis.sh>
2025-06-03 10:52:27 -0500

    feat(app): add tokio and tracing

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,3 +1,17 @@
 //! The server-side entrypoint for Rambit.
 
-fn main() {}
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::{EnvFilter, prelude::*};
+
+#[tokio::main]
+async fn main() {
+  let env_filter = EnvFilter::builder()
+    .with_default_directive(LevelFilter::INFO.into())
+    .from_env_lossy();
+  tracing_subscriber::registry()
+    .with(tracing_subscriber::fmt::layer())
+    .with(env_filter)
+    .init();
+
+  tracing::info!("starting app server");
+}
