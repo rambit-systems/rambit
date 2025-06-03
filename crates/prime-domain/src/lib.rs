602f1cd8eb56559a40e8651574a436571e441169
John Lewis <github@jlewis.sh>
2025-06-03 10:26:36 -0500

    feat(prime-domain): add repos to `PrimeDomainService`

--- crates/prime-domain/src/lib.rs
+++ crates/prime-domain/src/lib.rs
@@ -1,7 +1,33 @@
 //! Entrypoint for domain logic.
 
+use db::Database;
+use models::{Cache, Entry, Org, Store, User};
+
 /// The prime domain service type.
 #[derive(Debug, Clone)]
-pub struct PrimeDomainService {}
+pub struct PrimeDomainService {
+  org_repo:   Database<Org>,
+  user_repo:  Database<User>,
+  store_repo: Database<Store>,
+  entry_repo: Database<Entry>,
+  cache_repo: Database<Cache>,
+}
 
-impl PrimeDomainService {}
+impl PrimeDomainService {
+  /// Create a new [`PrimeDomainService`].
+  pub fn new(
+    org_repo: Database<Org>,
+    user_repo: Database<User>,
+    store_repo: Database<Store>,
+    entry_repo: Database<Entry>,
+    cache_repo: Database<Cache>,
+  ) -> Self {
+    Self {
+      org_repo,
+      user_repo,
+      store_repo,
+      entry_repo,
+      cache_repo,
+    }
+  }
+}
