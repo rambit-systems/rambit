e2d2befa456e766841a7b2297aff9696ecac9409
John Lewis <github@jlewis.sh>
2025-06-10 12:02:21 -0500

    refactor(app): isolate app_state module

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,13 +1,13 @@
 //! The server-side entrypoint for Rambit.
 
+mod app_state;
+
 use clap::Parser;
 use miette::{Context, Result};
-use prime_domain::{
-  PrimeDomainService,
-  db::{Database, kv},
-};
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::{EnvFilter, prelude::*};
+
+use self::app_state::AppState;
 
 /// The Rambit app CLI.
 #[derive(Parser)]
@@ -31,22 +31,13 @@ async fn main() -> Result<()> {
 
   tracing::info!("starting app server");
 
-  let kv_store_location = std::path::PathBuf::from(
-    std::env::var("REDB_STORE_PATH").unwrap_or("/tmp/picturepro-db".to_owned()),
-  );
-  let kv_store = kv::KeyValueStore::new_redb(&kv_store_location)?;
-
-  let org_db = Database::new_from_kv(kv_store.clone());
-  let user_db = Database::new_from_kv(kv_store.clone());
-  let store_db = Database::new_from_kv(kv_store.clone());
-  let entry_db = Database::new_from_kv(kv_store.clone());
-  let cache_db = Database::new_from_kv(kv_store);
-
-  let prime_domain_service =
-    PrimeDomainService::new(org_db, user_db, store_db, entry_db, cache_db);
+  let app_state = AppState::build()
+    .await
+    .context("failed to build app state")?;
 
   if args.migrate {
-    prime_domain_service
+    app_state
+      .prime_domain
       .migrate_test_data(false)
       .await
       .context("failed to migrate test data")?;
