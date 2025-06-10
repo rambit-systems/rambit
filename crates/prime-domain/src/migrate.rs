e20958e7d0efa2f30f28d2a946c36841fb1fe835
John Lewis <github@jlewis.sh>
2025-06-10 15:14:00 -0500

    fix(prime-domain): use explicit IDs for test migrations

--- crates/prime-domain/src/migrate.rs
+++ crates/prime-domain/src/migrate.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, str::FromStr};
 
 use miette::{Context, IntoDiagnostic, Result};
 use models::{
@@ -17,7 +17,7 @@ impl PrimeDomainService {
     let org = self
       .org_repo
       .create_model(Org {
-        id:   RecordId::new(),
+        id:   RecordId::from_str("01JXGXSB69BDHNFTSVG2EPW2M3").unwrap(),
         name: EntityName::new(StrictSlug::new("the-federation")),
       })
       .await
@@ -27,7 +27,7 @@ impl PrimeDomainService {
     let _user = self
       .user_repo
       .create_model(User {
-        id:   RecordId::new(),
+        id:   RecordId::from_str("01JXGXV4R6VCZWQ2DAYDWR1VXD").unwrap(),
         org:  org.id,
         name: HumanName::try_new("Jean-Luc Picard")
           .expect("failed to create name"),
@@ -39,7 +39,7 @@ impl PrimeDomainService {
     let albert_store = self
       .store_repo
       .create_model(Store {
-        id:          RecordId::new(),
+        id:          RecordId::from_str("01JXGXVF0MVQNGRM565YHM20BC").unwrap(),
         org:         org.id,
         credentials: match ephemeral_storage {
           true => {
@@ -59,7 +59,8 @@ impl PrimeDomainService {
     let _aaron_cache = self
       .cache_repo
       .create_model(Cache {
-        id:            RecordId::new(),
+        id:            RecordId::from_str("01JXGXVVE6J16590YJT3SP2P6M")
+          .unwrap(),
         org:           org.id,
         name:          EntityName::new(StrictSlug::new("aaron")),
         default_store: albert_store.id,
