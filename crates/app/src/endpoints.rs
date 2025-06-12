4a605a436c7a52dc53b2beac97ce8de090ae3d83
John Lewis <github@jlewis.sh>
2025-06-12 16:35:31 -0500

    refactor(app): move router construction to endpoint module

--- crates/app/src/endpoints.rs
+++ crates/app/src/endpoints.rs
@@ -1,9 +1,24 @@
-pub mod upload;
+mod upload;
 
-use axum::response::IntoResponse;
+use axum::{
+  Router,
+  response::IntoResponse,
+  routing::{get, post},
+};
+
+use self::upload::upload;
+use crate::app_state::AppState;
 
 #[axum::debug_handler]
 pub async fn root() -> impl IntoResponse {
   "You've reached the root endpoint of the Rambit API.\nYou probably meant to \
    go somewhere else."
+}
+
+pub fn router(app_state: AppState) -> Router {
+  axum::Router::new()
+    .route("/", get(root))
+    .route("/upload/{cache_name}/{path}", post(upload))
+    .route("/upload/{cache_name}/{path}/{target_store}", post(upload))
+    .with_state(app_state)
 }
