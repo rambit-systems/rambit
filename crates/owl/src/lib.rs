61e054f8bb4e4413ee24ae5d4c3a76ff43cd17c1
John Lewis <github@jlewis.sh>
2025-07-09 04:00:35 -0500

    feat(owl): scaffold basic interface

--- crates/owl/src/lib.rs
+++ crates/owl/src/lib.rs
@@ -1,1 +1,19 @@
+//! Tools to manipulate NARs.
 
+use belt::Belt;
+use models::NarIntrensicData;
+
+/// Interrogates a NAR and returns its intrensically known data.
+pub struct NarInterrogator;
+
+/// Possible failures of a NAR interrogation.
+pub enum InterrogatorError {}
+
+impl NarInterrogator {
+  /// Interrogate a NAR and return its intrensically known data.
+  pub async fn interrogate(
+    data: Belt,
+  ) -> Result<NarIntrensicData, InterrogatorError> {
+    todo!()
+  }
+}
