ac6ccc2ebd76d5a0e1ac20d474ff127587cb784f
John Lewis <github@jlewis.sh>
2025-07-31 17:05:03 -0500

    feat(site): make homepage responsive

--- crates/site-app/src/pages.rs
+++ crates/site-app/src/pages.rs
@@ -3,15 +3,15 @@ use leptos::prelude::*;
 #[component]
 pub fn HomePage() -> impl IntoView {
   view! {
-    <div class="container mx-auto flex flex-col gap-8 min-h-screen pb-8">
+    <div class="mx-4 sm:container sm:mx-auto flex flex-col gap-8 min-h-screen pb-8">
       <Navbar />
 
       <div class="bg-base-1 border border-base-6 rounded">
-        <div class="p-20 font-semibold flex flex-col gap-2">
-          <p class="text-lg text-product-11 uppercase">
+        <div class="p-6 sm:p-20 font-semibold flex flex-col gap-2">
+          <p class="text-sm sm:text-lg text-product-11 uppercase">
             "Welcome to Rambit Systems"
           </p>
-          <div class="text-6xl font-display tracking-tight">
+          <div class="text-4xl sm:text-6xl font-display tracking-tight">
             <p class="font-thin">
               "Integrate and"
             </p>
