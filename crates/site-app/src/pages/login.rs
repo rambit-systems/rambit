c799c2ab9d84993542eb8ffcfbdd71dd98f9ac41
John Lewis <github@jlewis.sh>
2025-08-04 10:27:59 -0500

    chore(app): split out components from login module

--- crates/site-app/src/pages/login.rs
+++ crates/site-app/src/pages/login.rs
@@ -1,5 +1,7 @@
 use leptos::prelude::*;
 
+use crate::components::{EnvelopeHeroIcon, InputField, LockClosedHeroIcon};
+
 #[component]
 pub fn LoginPage() -> impl IntoView {
   view! {
@@ -25,52 +27,5 @@ pub fn LoginPage() -> impl IntoView {
       </div>
     </div>
     <div class="flex-1" />
-  }
-}
-
-#[component]
-pub fn InputField(
-  id: &'static str,
-  label_text: &'static str,
-  input_type: &'static str,
-  placeholder: &'static str,
-  #[prop(optional)] before: Option<Children>,
-  #[prop(optional)] after: Option<Children>,
-) -> impl IntoView {
-  const OUTER_WRAPPER_CLASS: &str = "flex flex-col gap-1";
-  const LABEL_CLASS: &str = "text-base-11";
-  const INPUT_WRAPPER_CLASS: &str = "input-field max-w-80";
-  const INPUT_CLASS: &str = "w-full py-2 focus-visible:outline-none";
-
-  view! {
-    <div class=OUTER_WRAPPER_CLASS>
-      <label class=LABEL_CLASS for=id>{ label_text }</label>
-      <div class=INPUT_WRAPPER_CLASS>
-        { before.map(|b| b()) }
-        <input
-          class=INPUT_CLASS type=input_type
-          placeholder=placeholder id=id
-        />
-        { after.map(|a| a()) }
-      </div>
-    </div>
-  }
-}
-
-#[component]
-pub fn EnvelopeHeroIcon() -> impl IntoView {
-  view! {
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="size-6">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
-    </svg>
-  }
-}
-
-#[component]
-pub fn LockClosedHeroIcon() -> impl IntoView {
-  view! {
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="size-6">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
-    </svg>
   }
 }
