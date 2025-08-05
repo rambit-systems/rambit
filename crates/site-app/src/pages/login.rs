de27e9adf0bb26d3270c80908d927ae1d34ddc0b
John Lewis <github@jlewis.sh>
2025-08-05 17:12:40 -0500

    feat(app): fix [enter] submit on login form

--- crates/site-app/src/pages/login.rs
+++ crates/site-app/src/pages/login.rs
@@ -1,6 +1,9 @@
 use std::collections::HashMap;
 
-use leptos::{ev::Event, prelude::*};
+use leptos::{
+  ev::{Event, SubmitEvent},
+  prelude::*,
+};
 use models::dvf::{EmailAddress, EmailAddressError};
 
 use crate::{
@@ -12,10 +15,7 @@ use crate::{
 pub fn LoginPage() -> impl IntoView {
   view! {
     <div class="flex-1" />
-    <div class="p-8 self-stretch md:self-center md:w-xl elevation-flat flex flex-col gap-8">
-      <p class="title">"Login"</p>
-      <LoginIsland />
-    </div>
+    <LoginIsland />
     <div class="flex-1" />
   }
 }
@@ -86,7 +86,9 @@ pub fn LoginIsland() -> impl IntoView {
     }
   });
 
-  let submit_action = move |_| {
+  let submit_action = move |ev: SubmitEvent| {
+    ev.prevent_default();
+
     submit_touched.set(true);
 
     if email_hint().is_some() || password_hint().is_some() {
@@ -103,28 +105,35 @@ pub fn LoginIsland() -> impl IntoView {
   });
 
   view! {
-    <div class="flex flex-col gap-4">
-      <InputField
-        id="email" label_text="Email Address"
-        input_type="email" placeholder=""
-        before={ Box::new(|| view!{ <EnvelopeHeroIcon /> }.into_any()) }
-        input_signal=read_email output_signal=write_email
-        error_hint={MaybeProp::derive(move || submit_touched().then_some(email_hint()).flatten())}
-      />
-      <InputField
-        id="password" label_text="Password"
-        input_type="password" placeholder=""
-        before={ Box::new(|| view!{ <LockClosedHeroIcon /> }.into_any()) }
-        input_signal=read_password output_signal=write_password
-        error_hint={MaybeProp::derive(move || submit_touched().then_some(password_hint()).flatten())}
-      />
-    </div>
+    <form
+      on:submit=submit_action
+      class="p-8 self-stretch md:self-center md:w-xl elevation-flat flex flex-col gap-8"
+    >
+      <p class="title">"Login"</p>
+      <div class="flex flex-col gap-4">
+        <InputField
+          id="email" label_text="Email Address"
+          input_type="email" placeholder=""
+          before={ Box::new(|| view!{ <EnvelopeHeroIcon /> }.into_any()) }
+          input_signal=read_email output_signal=write_email
+          error_hint={MaybeProp::derive(move || submit_touched().then_some(email_hint()).flatten())}
+        />
+        <InputField
+          id="password" label_text="Password"
+          input_type="password" placeholder=""
+          before={ Box::new(|| view!{ <LockClosedHeroIcon /> }.into_any()) }
+          input_signal=read_password output_signal=write_password
+          error_hint={MaybeProp::derive(move || submit_touched().then_some(password_hint()).flatten())}
+        />
+      </div>
 
-    <div class="flex flex-row gap-2">
-      <button
-        class="btn btn-primary"
-        on:click=submit_action
-      >"Login"</button>
-    </div>
+      <div class="flex flex-row gap-2">
+        <input
+          type="submit"
+          class="btn btn-primary"
+          value="Log in"
+        />
+      </div>
+    </form>
   }
 }
