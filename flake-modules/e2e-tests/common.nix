a443e5ab8957f77d2a5ff308e05de5179bb1057e
John Lewis <github@jlewis.sh>
2025-08-06 11:32:31 -0500

    refactor(e2e): factor pieces out to common

--- flake-modules/e2e-tests/common.nix
+++ flake-modules/e2e-tests/common.nix
@@ -1,1 +1,36 @@
-{ ... }: { }
+{ config, ... }: {
+  # from ../../crates/prime-domain/src/migrate.rs
+  test-data = {
+    archive = ../../crates/owl/test/ky2wzr68im63ibgzksbsar19iyk861x6-bat-0.25.0;
+    store-path = "ky2wzr68im63ibgzksbsar19iyk861x6-bat-0.25.0";
+    deriver = "4yz8qa58nmysad5w88rgdhq15rkssqr6-bat-0.25.0";
+    deriver-system = "aarch64-linux";
+    email = "jpicard@federation.gov";
+    password = "password";
+    cache = "aaron";
+    store = "albert";
+  };
+  
+  grid-node = {
+    networking.firewall.allowedTCPPorts = [ 3000 ];
+
+    systemd.services.grid = {
+      description = "Grid Server";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${config.packages.grid}/bin/grid --migrate --host 0.0.0.0 --no-secure-cookies";
+      };
+    };
+  };
+
+  client-node = { pkgs, ... }: {
+    environment.systemPackages = with pkgs; [
+      curl jq
+      config.packages.cli
+    ];
+    nix.extraOptions = "experimental-features = nix-command";
+  };
+}
