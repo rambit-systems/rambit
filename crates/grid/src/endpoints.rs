921b5bf2e55be372941de5556b481d142151d6b7
John Lewis <github@jlewis.sh>
2025-08-07 16:31:09 -0500

    feat(grid): add API fallback

--- crates/grid/src/endpoints.rs
+++ crates/grid/src/endpoints.rs
@@ -7,6 +7,7 @@ mod upload;
 
 use axum::{
   Router,
+  http::StatusCode,
   response::IntoResponse,
   routing::{get, post},
 };
@@ -26,6 +27,11 @@ pub async fn root() -> impl IntoResponse {
    go somewhere else."
 }
 
+#[axum::debug_handler]
+pub async fn fallback() -> impl IntoResponse {
+  (StatusCode::NOT_FOUND, "endpoint not found")
+}
+
 pub fn router() -> Router<AppState> {
   axum::Router::new()
     .route("/", get(root))
@@ -35,4 +41,5 @@ pub fn router() -> Router<AppState> {
     .route("/c/{cache_name}/nix-cache-info", get(nix_cache_info))
     .route("/c/{cache_name}/download/{store_path}", get(download))
     .route("/c/{cache_name}/{digest_with_suffix}", get(narinfo))
+    .fallback(fallback)
 }
