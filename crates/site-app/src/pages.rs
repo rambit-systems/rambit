5eaf7f8bc5289af41ac5bb669e1a6fc234bba743
John Lewis <github@jlewis.sh>
2025-08-09 14:24:37 -0500

    feat(app): add unauthorized page

--- crates/site-app/src/pages.rs
+++ crates/site-app/src/pages.rs
@@ -3,5 +3,8 @@ mod homepage;
 mod login;
 mod logout;
 mod signup;
+mod unauthorized;
 
-pub use self::{dashboard::*, homepage::*, login::*, logout::*, signup::*};
+pub use self::{
+  dashboard::*, homepage::*, login::*, logout::*, signup::*, unauthorized::*,
+};
