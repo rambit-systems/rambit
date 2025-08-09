c01d72ce0b36803af0aa0310937e72526e04e92a
John Lewis <github@jlewis.sh>
2025-08-09 14:24:37 -0500

    feat(app): add access control to dashboard page

--- crates/site-app/src/pages/dashboard.rs
+++ crates/site-app/src/pages/dashboard.rs
@@ -1,10 +1,40 @@
 use leptos::prelude::*;
+use leptos_router::hooks::use_params_map;
+use models::{dvf::RecordId, AuthUser};
+
+use crate::pages::UnauthorizedPage;
 
 #[component]
 pub fn DashboardPage() -> impl IntoView {
-  view! {
-    <div class="grid gap-4 h-full">
+  let params = use_params_map();
+  let authorized = Memo::new(move |_| {
+    let allowed_orgs = use_context::<AuthUser>()
+      .map(|au| au.iter_orgs().collect::<Vec<_>>())
+      .unwrap_or_default();
+    let Some(requested_org) = params()
+      .get("org")
+      .expect("missing org path param")
+      .parse::<RecordId<_>>()
+      .ok()
+    else {
+      return false;
+    };
+    allowed_orgs.contains(&requested_org)
+  });
 
+  move || match authorized() {
+    true => view! { <DashboardInner /> }.into_any(),
+    false => view! { <UnauthorizedPage /> }.into_any(),
+  }
+}
+
+#[component]
+fn DashboardInner() -> impl IntoView {
+  view! {
+    <div class="grid gap-4 h-full grid-cols-2 grid-rows-2">
+      <div class="elevation-flat">
+        <p class="title">"Dashboard"</p>
+      </div>
     </div>
   }
 }
