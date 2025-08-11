8dbd833d22212df194928f6c321f14e3c5a9dd2e
John Lewis <github@jlewis.sh>
2025-08-10 22:07:29 -0500

    feat(app): restructure dashboard module

---
+++ crates/site-app/src/pages/dashboard/cache.rs
@@ -0,0 +1,47 @@
+use leptos::prelude::*;
+use models::{dvf::RecordId, Cache, Org};
+
+#[component]
+pub(super) fn CacheDashboardTile(org: RecordId<Org>) -> impl IntoView {
+  let caches_in_org = crate::resources::caches_in_org(org);
+  let suspend = move || {
+    Suspend::new(async move {
+      match caches_in_org.await {
+        Ok(caches) => view! { <CacheTable caches=caches /> }.into_any(),
+        Err(e) => view! { { format!("Error: {e}") } }.into_any(),
+      }
+    })
+  };
+
+  view! {
+    <div class="p-6 elevation-flat flex flex-col gap-4">
+      <p class="title">"Caches"</p>
+
+      <Suspense fallback=move || view! { "Loading..." }>
+        { suspend }
+      </Suspense>
+    </div>
+  }
+}
+
+#[component]
+fn CacheTable(caches: Vec<Cache>) -> impl IntoView {
+  view! {
+    <table class="table">
+      <thead>
+        <th>"Name"</th>
+        <th>"Visibility"</th>
+        <th>"Default Cache"</th>
+      </thead>
+      <tbody>
+        { move || caches.iter().map(|c| view! {
+          <tr>
+            <th scope="row">{ c.name.to_string() }</th>
+            <td>{ c.visibility.to_string() }</td>
+            <td>{ c.default_store.to_string() }</td>
+          </tr>
+        }).collect::<Vec<_>>() }
+      </tbody>
+    </table>
+  }
+}
