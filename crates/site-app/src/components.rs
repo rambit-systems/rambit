7dd846a387a7224167627586d29e2931071aed6c
John Lewis <github@jlewis.sh>
2025-08-10 22:09:54 -0500

    feat(app): add item-links module

--- crates/site-app/src/components.rs
+++ crates/site-app/src/components.rs
@@ -1,5 +1,6 @@
 mod icons;
 mod input_field;
+mod item_links;
 mod navbar;
 
-pub use self::{icons::*, input_field::*, navbar::*};
+pub use self::{icons::*, input_field::*, item_links::*, navbar::*};
