976ba0b35cdcf262659020977d7cf7726afc688c
John Lewis <github@jlewis.sh>
2025-08-15 14:39:30 -0500

    feat(auth-domain): add `switch_active_org` method

--- crates/auth-domain/src/lib.rs
+++ crates/auth-domain/src/lib.rs
@@ -3,7 +3,10 @@
 
 use axum_login::AuthUser as AxumLoginAuthUser;
 pub use axum_login::AuthnBackend;
-use db::{Database, FetchModelByIndexError, FetchModelError, kv::LaxSlug};
+use db::{
+  Database, FetchModelByIndexError, FetchModelError, PatchModelError,
+  kv::LaxSlug,
+};
 use miette::{IntoDiagnostic, miette};
 use models::{
   AuthUser, Org, OrgIdent, User, UserAuthCredentials,
@@ -65,6 +68,23 @@ pub enum AuthenticationError {
   PasswordHashing(miette::Report),
 }
 
+/// An error that occurs while updating a user's active org.
+#[derive(Debug, thiserror::Error, miette::Diagnostic)]
+pub enum UpdateActiveOrgError {
+  /// Indicates that an error occurred while fetching the user.
+  #[error("Failed to fetch user")]
+  FetchError(#[from] FetchModelError),
+  /// Indicates that the user does not exist.
+  #[error("Failed to find user: {0}")]
+  MissingUser(RecordId<User>),
+  /// Indicates that the org supplied could not be switched to.
+  #[error("Failed to switch to org: {0}")]
+  InvalidOrg(RecordId<Org>),
+  /// Indicates that an error occurred while patching the user record.
+  #[error("Failed to patch user")]
+  PatchError(#[from] PatchModelError),
+}
+
 impl AuthDomainService {
   /// Fetch a [`User`] by ID.
   #[instrument(skip(self))]
@@ -88,6 +108,35 @@ impl AuthDomainService {
         EitherSlug::Lax(LaxSlug::new(email.as_ref())),
       )
       .await
+  }
+
+  /// Switch a [`User`]'s active org.
+  #[instrument(skip(self))]
+  pub async fn switch_active_org(
+    &self,
+    user: RecordId<User>,
+    new_active_org: RecordId<Org>,
+  ) -> Result<Option<RecordId<Org>>, UpdateActiveOrgError> {
+    let user = self
+      .user_repo
+      .fetch_model_by_id(user)
+      .await?
+      .ok_or(UpdateActiveOrgError::MissingUser(user))?;
+
+    let new_index = user
+      .iter_orgs()
+      .position(|o| o == new_active_org)
+      .ok_or(UpdateActiveOrgError::InvalidOrg(new_active_org))?;
+
+    self
+      .user_repo
+      .patch_model(user.id, User {
+        active_org_index: new_index as _,
+        ..user
+      })
+      .await?;
+
+    Ok(Some(new_active_org))
   }
 
   /// Sign up a [`User`].
