0179b7f8f293cc83cdc877a77cc43f391d9d8ebe
John Lewis <github@jlewis.sh>
2025-08-17 15:49:56 -0500

    feat(app): add title to dashboard page

--- crates/site-app/src/pages/dashboard.rs
+++ crates/site-app/src/pages/dashboard.rs
@@ -16,9 +16,10 @@ pub fn DashboardPage() -> impl IntoView {
   let org: RecordId<Org> = expect_context();
 
   view! {
-    <div class="flex flex-row items-start gap-4">
-      <CurrentOrgTile org=org />
-      <div class="flex-1 grid gap-4 grid-cols-2">
+    <div class="grid grid-cols-[320px_minmax(0,_1fr)] gap-4 place-items-start">
+      <p class="title col-start-2">"Dashboard"</p>
+      <CurrentOrgTile org={org} {..} class="w-80" />
+      <div class="flex-1 grid gap-4 grid-cols-2 place-self-stretch">
         <div class="col-span-2 p-6 elevation-flat flex flex-col gap-4">
           <EntryTable org=org />
         </div>
--- crates/site-app/src/pages/dashboard/current_org.rs
+++ crates/site-app/src/pages/dashboard/current_org.rs
@@ -10,7 +10,7 @@ pub(super) fn CurrentOrgTile(org: RecordId<Org>) -> impl IntoView {
   let descriptor = org_hook.descriptor();
 
   view! {
-    <div class="w-80 p-4 elevation-flat flex flex-col gap-4">
+    <div class="p-4 elevation-flat flex flex-col gap-4">
       <div class="flex flex-col leading-none">
         <p class="text-xl">"org"</p>
         <p class="text-3xl text-base-12">
