2fbd39a461e2dc8c4f248f3c43363cf293374d8b
John Lewis <github@jlewis.sh>
2025-08-24 17:18:18 -0500

    feat(app): parse entry ID and protect entry page

--- crates/site-app/src/pages/entry.rs
+++ crates/site-app/src/pages/entry.rs
@@ -1,7 +1,25 @@
 use leptos::prelude::*;
+use leptos_router::hooks::use_params_map;
+use models::{dvf::RecordId, Entry};
+
+use crate::pages::UnauthorizedPage;
 
 #[component]
 pub fn EntryPage() -> impl IntoView {
+  let params = use_params_map();
+  let requested_entry = params()
+    .get("entry")
+    .expect("missing entry path param")
+    .parse::<RecordId<_>>()
+    .ok();
+
+  requested_entry
+    .map(|e| view! { <EntryTile id=e /> }.into_any())
+    .unwrap_or(view! { <UnauthorizedPage /> }.into_any())
+}
+
+#[component]
+fn EntryTile(id: RecordId<Entry>) -> impl IntoView {
   view! {
     <div class="elevation-flat p-4 flex flex-col gap-4">
       <p class="title">"Entry"</p>
