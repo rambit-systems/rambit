c0c40d66ac3ffc102700e4ed4a2728f12c2e76b7
John Lewis <github@jlewis.sh>
2025-09-03 12:44:18 -0700

    fix(app): archive `CacheHook`

--- crates/site-app/src/hooks.rs
+++ crates/site-app/src/hooks.rs
@@ -2,4 +2,5 @@ mod cache_hook;
 mod entry_hook;
 mod org_hook;
 
-pub use self::{cache_hook::*, entry_hook::*, org_hook::*};
+// pub use self::cache_hook::*;
+pub use self::{entry_hook::*, org_hook::*};
