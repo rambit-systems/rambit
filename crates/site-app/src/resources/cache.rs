4eafe4a60506bad4cd8e45d6ac1bccbd01040583
John Lewis <github@jlewis.sh>
2025-09-05 14:57:21 -0700

    feat(app): introduce `cache_name_is_available` resource

--- crates/site-app/src/resources/cache.rs
+++ crates/site-app/src/resources/cache.rs
@@ -1,9 +1,13 @@
 use leptos::prelude::*;
 use leptos_fetch::{QueryClient, QueryScope};
-use models::{dvf::RecordId, model::Model, Cache, Org, PvCache};
+use models::{
+  dvf::{EntityName, RecordId, StrictSlug},
+  model::Model,
+  Cache, Org, PvCache,
+};
 
 #[cfg(feature = "ssr")]
-use crate::resources::authorize_for_org;
+use crate::resources::{authenticate, authorize_for_org};
 
 pub fn cache(
   key: impl Fn() -> RecordId<Cache> + Send + Sync + 'static,
@@ -87,4 +91,32 @@ pub async fn fetch_caches_in_org(
   }
 
   Ok(models)
+}
+
+pub fn cache_name_is_available_query_scope(
+) -> QueryScope<String, Result<bool, ServerFnError>> {
+  QueryScope::new(check_if_cache_name_is_available)
+    .with_invalidation_link(move |_| [Cache::TABLE_NAME])
+}
+
+#[server(prefix = "/api/sfn")]
+pub async fn check_if_cache_name_is_available(
+  name: String,
+) -> Result<bool, ServerFnError> {
+  use prime_domain::PrimeDomainService;
+
+  authenticate()?;
+
+  let prime_domain_service: PrimeDomainService = expect_context();
+
+  let occupied = prime_domain_service
+    .fetch_cache_by_name(EntityName::new(StrictSlug::new(name)))
+    .await
+    .map_err(|e| {
+      tracing::error!("failed to fetch cache by name: {e}");
+      ServerFnError::new("internal error")
+    })?
+    .is_some();
+
+  Ok(!occupied)
 }
