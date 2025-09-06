de3291e2cb31e55deba6badcff55e509682d5996
John Lewis <github@jlewis.sh>
2025-09-06 16:55:24 -0700

    feat(app): introduce `SignupHook`

--- crates/site-app/src/hooks.rs
+++ crates/site-app/src/hooks.rs
@@ -1,6 +1,7 @@
 mod cache_hook;
 mod entry_hook;
 mod org_hook;
+mod signup_hook;
 
 // pub use self::cache_hook::*;
-pub use self::{entry_hook::*, org_hook::*};
+pub use self::{entry_hook::*, org_hook::*, signup_hook::*};
