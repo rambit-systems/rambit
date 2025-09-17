f94e37008498d70e63b59bf364263738a04b31be
John Lewis <github@jlewis.sh>
2025-09-17 14:45:18 -0700

    feat(app): add definition tracking to `OrgHook`

--- crates/site-app/src/hooks/org_hook.rs
+++ crates/site-app/src/hooks/org_hook.rs
@@ -1,3 +1,5 @@
+use std::panic::Location;
+
 use leptos::prelude::*;
 use leptos_fetch::QueryClient;
 use models::{dvf::RecordId, AuthUser, Org, PvOrg};
@@ -6,38 +8,75 @@ use crate::{pages::RequestedOrg, resources::org::org_query_scope};
 
 #[derive(Clone)]
 pub struct OrgHook {
-  key:      Callback<(), RecordId<Org>>,
-  resource: Resource<Result<Option<PvOrg>, ServerFnError>>,
-  user:     AuthUser,
+  #[cfg(debug_assertions)]
+  defined_at: &'static Location<'static>,
+  key:        Callback<(), RecordId<Org>>,
+  resource:   Resource<Result<Option<PvOrg>, ServerFnError>>,
+  user:       AuthUser,
 }
 
 impl OrgHook {
-  /// Creates a new [`OrgHook`]. Requires [`AuthUser`] in context.
-  pub fn new(
+  fn new_inner(
     key: impl Fn() -> RecordId<Org> + Copy + Send + Sync + 'static,
+    defined_at: &'static Location,
   ) -> Self {
-    let auth_user = expect_context();
-    let client = expect_context::<QueryClient>();
+    let auth_user = use_context().unwrap_or_else(|| {
+      panic!(
+        "context of type `AuthUser` was not found by `OrgHook` defined at {}",
+        defined_at
+      )
+    });
+    let client = use_context::<QueryClient>().unwrap_or_else(|| {
+      panic!(
+        "context of type `QueryClient` was not found by `OrgHook` defined at \
+         {}",
+        defined_at
+      )
+    });
     let resource = client.resource(org_query_scope(), key);
 
     OrgHook {
+      #[cfg(debug_assertions)]
+      defined_at,
       key: Callback::new(move |_| key()),
       resource,
       user: auth_user,
     }
   }
 
+  /// Creates a new [`OrgHook`]. Requires [`AuthUser`] in context.
+  #[track_caller]
+  pub fn new(
+    key: impl Fn() -> RecordId<Org> + Copy + Send + Sync + 'static,
+  ) -> Self {
+    Self::new_inner(key, Location::caller())
+  }
+
   /// Creates a new [`OrgHook`] using the [`AuthUser`]'s active org.
+  #[track_caller]
   pub fn new_active() -> Self {
-    let auth_user = expect_context::<AuthUser>();
+    let defined_at = Location::caller();
+    let auth_user = use_context::<AuthUser>().unwrap_or_else(|| {
+      panic!(
+        "context of type `AuthUser` was not found by `OrgHook` defined at \
+         {defined_at}",
+      )
+    });
     let active_org = auth_user.active_org();
-    Self::new(move || active_org)
+    Self::new_inner(move || active_org, defined_at)
   }
 
   /// Creates a new [`OrgHook`] using the [`RequestedOrg`] in context.
+  #[track_caller]
   pub fn new_requested() -> Self {
-    let RequestedOrg(requested_org) = expect_context();
-    Self::new(move || requested_org)
+    let defined_at = Location::caller();
+    let RequestedOrg(requested_org) = use_context().unwrap_or_else(|| {
+      panic!(
+        "context of type `RequestedOrg` was not found by `OrgHook` defined at \
+         {defined_at}",
+      )
+    });
+    Self::new_inner(move || requested_org, defined_at)
   }
 
   pub fn key(
@@ -78,5 +117,18 @@ impl OrgHook {
         }
       }
     })
+  }
+}
+
+impl DefinedAt for OrgHook {
+  fn defined_at(&self) -> Option<&'static Location<'static>> {
+    #[cfg(debug_assertions)]
+    {
+      Some(self.defined_at)
+    }
+    #[cfg(not(debug_assertions))]
+    {
+      None
+    }
   }
 }
