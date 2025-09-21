a37439c3f718663334094ba5effb9ee0fe09cc3d
John Lewis <github@jlewis.sh>
2025-09-21 15:50:50 -0700

    feat(app): only run `refetch_while_focused` on client

--- crates/site-app/src/components/refetch_while_focused.rs
+++ crates/site-app/src/components/refetch_while_focused.rs
@@ -14,24 +14,27 @@ pub fn refetch_while_focused<
   query_scope: QueryScope<K, R>,
   period: Duration,
 ) {
-  let query_client = expect_context::<QueryClient>();
-  let period_ms = Signal::stored(period.as_millis() as u64);
+  #[cfg(feature = "hydrate")]
+  {
+    let query_client = expect_context::<QueryClient>();
+    let period_ms = Signal::stored(period.as_millis() as u64);
 
-  let invalidate = {
-    let query_scope = query_scope.clone();
-    move || {
-      query_client.invalidate_query(query_scope.clone(), key_fn());
-    }
-  };
-
-  let focused = use_window_focus();
-
-  let _ = use_interval_fn(
-    move || {
-      if focused() {
-        invalidate()
+    let invalidate = {
+      let query_scope = query_scope.clone();
+      move || {
+        query_client.invalidate_query(query_scope.clone(), key_fn());
       }
-    },
-    period_ms,
-  );
+    };
+
+    let focused = use_window_focus();
+
+    let _ = use_interval_fn(
+      move || {
+        if focused() {
+          invalidate()
+        }
+      },
+      period_ms,
+    );
+  }
 }
