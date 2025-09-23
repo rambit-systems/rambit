daae4237630ab28d525d8151a5c8ed2cdd5b52ee
John Lewis <github@jlewis.sh>
2025-09-23 15:51:03 -0700

    feat(app): add link for `CreateOrgPage`

--- crates/site-app/src/pages/dashboard/requested_org.rs
+++ crates/site-app/src/pages/dashboard/requested_org.rs
@@ -10,7 +10,13 @@ pub(super) fn RequestedOrgTile() -> impl IntoView {
   view! {
     <div class="p-4 elevation-flat flex flex-col gap-4">
       <div class="flex flex-col leading-none">
-        <p class="text-xl">"org"</p>
+        <div class="flex flex-row gap-2 items-center justify-between">
+          <p class="text-xl">"org"</p>
+          <a
+            href="/org/create_org"
+            class="text-link text-link-primary"
+          >"Create Org..."</a>
+        </div>
         <p class="text-3xl text-base-12">
           <Suspense fallback=|| "[loading]">
             { move || Suspend::new(descriptor) }
