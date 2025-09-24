8f4ce368d114fe2aa8203ced921ff48ac378c8fe
John Lewis <github@jlewis.sh>
2025-09-24 14:34:47 -0700

    feat(app): add general popover component

--- crates/site-app/src/components.rs
+++ crates/site-app/src/components.rs
@@ -5,10 +5,11 @@ mod icons;
 mod input_field;
 mod item_links;
 mod navbar;
+mod popover;
 mod refetch_while_focused;
 mod store_path;
 
 pub use self::{
   create_button::*, data_table::*, icons::*, input_field::*, item_links::*,
-  navbar::*, refetch_while_focused::*, store_path::*,
+  navbar::*, popover::*, refetch_while_focused::*, store_path::*,
 };
