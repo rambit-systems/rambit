9e64afdf523b8e1c056cb72f6dd1c89a79d8ecb4
John Lewis <github@jlewis.sh>
2025-09-29 16:20:34 -0700

    feat(app): add `Entry` to entry page title

--- crates/site-app/src/pages/entry/title_tile.rs
+++ crates/site-app/src/pages/entry/title_tile.rs
@@ -9,9 +9,12 @@ pub(crate) fn TitleTile(store_path: StorePath<String>) -> impl IntoView {
   view! {
     <div class="p-6 elevation-flat flex flex-row gap-2 items-center">
       <p class="text-base-12 text-xl">
+        <span class="font-bold">
+          "Entry: "
+        </span>
         { path.clone() }
       </p>
-      <CopyButton copy_content={path} {..} class="size-6" />
+      <CopyButton copy_content={path} {..} class="size-5" />
     </div>
   }
 }
