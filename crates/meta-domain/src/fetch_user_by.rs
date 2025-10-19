080f04794e16becbf8b41dbc68ea2ac47d00420c
John Lewis <github@jlewis.sh>
2025-10-19 12:10:04 -0700

    chore(docs): fix docs errors

--- crates/meta-domain/src/fetch_user_by.rs
+++ crates/meta-domain/src/fetch_user_by.rs
@@ -4,7 +4,7 @@ use models::{User, UserUniqueIndexSelector, dvf::EmailAddress};
 use crate::MetaService;
 
 impl MetaService {
-  /// Fetch a [`User`] by [`EmailAddress`](EmailAddress).
+  /// Fetch a [`User`] by [`EmailAddress`].
   pub async fn fetch_user_by_email(
     &self,
     email: EmailAddress,
