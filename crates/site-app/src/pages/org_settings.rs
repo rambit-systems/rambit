979f36b23b1224f19a5f447b1ca3597dde0b3205
John Lewis <github@jlewis.sh>
2025-11-11 16:13:57 -0800

    feat(app): make org settings nav buttons reactive to subpage

--- crates/site-app/src/pages/org_settings.rs
+++ crates/site-app/src/pages/org_settings.rs
@@ -26,17 +26,37 @@ pub fn OrgSettingsPage() -> impl IntoView {
 #[component]
 pub fn NavButtons() -> impl IntoView {
   let org_hook = OrgHook::new_requested();
+  let current_url = leptos_router::hooks::use_url();
+  let current_path = Signal::derive(move || current_url().path().to_owned());
+
   let settings_url = org_hook.settings_url();
   let settings_billing_url =
     Signal::derive(move || format!("{}/billing", settings_url()));
 
+  let is_generator =
+    move |s: Signal<String>| Signal::derive(move || current_path() == s());
+  let is_overview = is_generator(settings_url.into());
+  let is_billing = is_generator(settings_billing_url);
+
+  let class_generator = move |is: Signal<bool>| {
+    Signal::derive(move || {
+      if is() {
+        "btn btn-secondary btn-tight"
+      } else {
+        "btn-link btn-link-secondary btn-link-tight"
+      }
+    })
+  };
+  let overview_class = class_generator(is_overview);
+  let billing_class = class_generator(is_billing);
+
   view! {
     <div class="elevation-flat p-4 flex flex-col gap-2 w-64">
-      <a href=settings_url class="btn btn-secondary btn-tight">
+      <a href=settings_url class=overview_class>
         <Cog6ToothHeroIcon {..} class="size-5 stroke-base-11 stroke-[2.0]" />
         "Overview"
       </a>
-      <a href=settings_billing_url class="btn btn-secondary btn-tight">
+      <a href=settings_billing_url class=billing_class>
         <CreditCardHeroIcon {..} class="size-5 stroke-base-11 stroke-[2.0]" />
         "Billing"
       </a>
