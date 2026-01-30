cfe1ccbe6338b53e8f15915cc38f0968ee3e6905
John Lewis <github@jlewis.sh>
2026-01-30 15:22:16 -0800

    chore(app): nuke `CreateCacheHook`

--- crates/site-app/src/hooks.rs
+++ crates/site-app/src/hooks.rs
@@ -1,12 +1,9 @@
 //! Hooks are reusable pieces of reactive logic extracted from render contexts.
 
 mod cache_hook;
-mod create_cache_hook;
 mod delete_entry_hook;
 mod entry_hook;
 mod org_hook;
 
 // pub use self::cache_hook::*;
-pub use self::{
-  create_cache_hook::*, delete_entry_hook::*, entry_hook::*, org_hook::*,
-};
+pub use self::{delete_entry_hook::*, entry_hook::*, org_hook::*};
