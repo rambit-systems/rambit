751eac09ff0444362e1280dc6d79f77d4ceff933
John Lewis <github@jlewis.sh>
2026-01-30 09:56:50 -0800

    refactor(app): isolate create_org validation module

--- crates/site-app/src/pages/create_org.rs
+++ crates/site-app/src/pages/create_org.rs
@@ -1,14 +1,14 @@
+mod validate;
+
 use leptos::prelude::*;
-use leptos_fetch::QueryClient;
 use leptos_router::{
   any_nested_route::IntoAnyNestedRoute, components::Route, path,
 };
-use models::EntityName;
 
+use self::validate::OrgNameValidation;
 use crate::{
   components::{form_layout::*, BuildingOffice2HeroIcon, LoadingCircle},
   pages::protect,
-  resources::org::org_name_is_available_query_scope,
 };
 
 const ORG_DESCRIPTION: &str =
@@ -96,65 +96,4 @@ fn CreateOrgPage() -> impl IntoView {
       </GridRow>
     </form>
   }
-}
-
-#[component]
-fn OrgNameValidation() -> impl IntoView {
-  let form_data = leptos_router::hooks::use_query_map().get();
-  let Some(name) = form_data.get(NAME_FIELD_NAME) else {
-    return const_format::formatcp!(
-      "The validation request did not contain the \"{NAME_FIELD_NAME}\" field \
-       :/"
-    )
-    .into_any();
-  };
-
-  if name.is_empty() {
-    return ().into_any();
-  }
-
-  let sanitized_name = EntityName::new(name.clone());
-
-  let sanitization_hint = (sanitized_name.as_ref() != name).then_some(view! {
-    <p class="text-warn-11 text-sm">
-      "This name will be converted to \""{ sanitized_name.as_ref() }"\"".
-    </p>
-  });
-
-  let is_available_resource = expect_context::<QueryClient>().resource(
-    org_name_is_available_query_scope(),
-    Signal::stored(sanitized_name.to_string()),
-  );
-  let availability_hint_suspend = move || {
-    Suspend::new(async move {
-      match is_available_resource.await {
-        Ok(true) => view! {
-          <p class="text-sm">
-            "This name is available."
-          </p>
-        }
-        .into_any(),
-        Ok(false) => view! {
-          <p class="text-critical-11 text-sm">
-            "This name is not available."
-          </p>
-        }
-        .into_any(),
-        Err(_) => view! {
-          <p class="text-critical-11 text-sm">
-            "Unable to check org name availability"
-          </p>
-        }
-        .into_any(),
-      }
-    })
-  };
-
-  view! {
-    <div class="flex flex-col">
-      { sanitization_hint }
-      { availability_hint_suspend }
-    </div>
-  }
-  .into_any()
 }
