35dc03766270ee1b87906cc2dc03b998a9addc34
John Lewis <github@jlewis.sh>
2026-03-13 12:09:46 -0700

    fix(app): make create store page more clear

--- crates/app/src/pages/create_store_page/credentials_input.rs
+++ crates/app/src/pages/create_store_page/credentials_input.rs
@@ -10,51 +10,64 @@ pub(super) const BUCKET_FIELD_NAME: &str = "bucket";
 pub(super) const ENDPOINT_FIELD_NAME: &str = "endpoint";
 
 pub(super) fn credentials_input() -> Markup {
+  let access_key_field = credentials_field(
+    ACCESS_KEY_FIELD_NAME,
+    "Access Key",
+    "RGVjb2RlIHRoZSBzZWNyZXQga2V5",
+    "text",
+    html! { div class="size-4 shrink-0 stroke-base-11 stroke-[2.0]" { (key_hero_icon()) } },
+  );
+  let secret_access_key_field = credentials_field(
+    SECRET_ACCESS_KEY_FIELD_NAME,
+    "Secret Access Key",
+    "TXkgbXksIGFyZW4ndCB5b3UgcXVpdGUgYSBjdXJpb3VzIG9uZS4gYmFzZTY0QHJhbWJpdC5hcHA",
+    "password",
+    html! { div class="size-4 shrink-0 stroke-base-11 stroke-[2.0]" { (key_hero_icon()) } },
+  );
+  let bucket_field = credentials_field(
+    BUCKET_FIELD_NAME,
+    "Bucket",
+    "rambit-public-cache or similar",
+    "text",
+    html! { div class="size-4 shrink-0 stroke-base-11 stroke-[2.0]" { (archive_box_hero_icon()) } },
+  );
+  let endpoint_field = credentials_field(
+    ENDPOINT_FIELD_NAME,
+    "Endpoint",
+    "https://...",
+    "text",
+    html! { div class="size-4 shrink-0 stroke-base-11 stroke-[2.0]" { (globe_alt_hero_icon()) } },
+  );
+
   html! {
     div class="flex flex-col gap-2" {
-      (credentials_field(
-        ACCESS_KEY_FIELD_NAME,
-        "Access Key",
-        "text",
-        html! { div class="size-4 shrink-0 stroke-base-11 stroke-[2.0]" { (key_hero_icon()) } },
-      ))
-      (credentials_field(
-        SECRET_ACCESS_KEY_FIELD_NAME,
-        "Secret Access Key",
-        "password",
-        html! { div class="size-4 shrink-0 stroke-base-11 stroke-[2.0]" { (key_hero_icon()) } },
-      ))
-      (credentials_field(
-        BUCKET_FIELD_NAME,
-        "Bucket",
-        "text",
-        html! { div class="size-4 shrink-0 stroke-base-11 stroke-[2.0]" { (archive_box_hero_icon()) } },
-      ))
-      (credentials_field(
-        ENDPOINT_FIELD_NAME,
-        "Endpoint",
-        "text",
-        html! { div class="size-4 shrink-0 stroke-base-11 stroke-[2.0]" { (globe_alt_hero_icon()) } },
-      ))
+      (access_key_field)
+      (secret_access_key_field)
+      (bucket_field)
+      (endpoint_field)
     }
   }
 }
 
 fn credentials_field(
   name: &str,
+  label: &str,
   placeholder: &str,
   input_type: &str,
   icon: Markup,
 ) -> Markup {
   html! {
-    label class="input-field" {
-      (icon)
-      input
-        class="w-full py-2 focus-visible:outline-none"
-        type=(input_type)
-        placeholder=(placeholder)
-        name=(name)
-        required;
+    label class="flex flex-col" {
+      p class="text-sm" { (label) }
+      div class="input-field" {
+        (icon)
+        input
+          class="w-full py-2 focus-visible:outline-none"
+          type=(input_type)
+          placeholder=(placeholder)
+          name=(name)
+          required;
+      }
     }
   }
 }
